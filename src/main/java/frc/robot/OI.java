fd70ad3e6b0d2cce2de56c5e740b23e542241914
royaboya <roychen1716@gmail.com>
2022-01-14 15:57:54 -0500

    OI Joystick left/right

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
-public class OI {
-    
+import frc.robot.PortMap;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class OI{
+    public Joystick JoystickLeft = new Joystick(PortMap.JOYSTICK_LEFT);
+    public Joystick JoysticRight = new Joystick(PortMap.JOYSTICK_RIGHT);
 }
