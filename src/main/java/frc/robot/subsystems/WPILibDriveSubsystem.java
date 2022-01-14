7bd7a03cdf200262ee8df414cd0732d9abe10fbc
miscsb <buenas@bxscience.edu>
2022-01-14 16:41:05 -0500

    fixed imports + constructors

--- src/main/java/frc/robot/subsystems/WPILibDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/WPILibDriveSubsystem.java
@@ -9,21 +9,21 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.SpeedController;
 
 public class WPILibDriveSubsystem extends SubsystemBase {
     protected DifferentialDrive drive;
-    private MotorController lDrive, rDrive;
+    private SpeedController lDrive, rDrive;
 
     public WPILibDriveSubsystem() {
         this.lDrive = new SpeedControllerGroup(
-            new CANSparkMax(PortMap.LEFT_FRONT_SPARK),
-            new CANSparkMax(PortMap.LEFT_MIDDLE_SPARK),
-            new CANSparkMax(PortMap.LEFT_BACK_SPARK));
+            new CANSparkMax(PortMap.LEFT_FRONT_SPARK, MotorType.kBrushless),
+            new CANSparkMax(PortMap.LEFT_MIDDLE_SPARK, MotorType.kBrushless),
+            new CANSparkMax(PortMap.LEFT_BACK_SPARK, MotorType.kBrushless));
         this.rDrive = new SpeedControllerGroup(
-            new CANSparkMax(PortMap.RIGHT_FRONT_SPARK),
-            new CANSparkMax(PortMap.RIGHT_MIDDLE_SPARK),
-            new CANSparkMax(PortMap.RIGHT_BACK_SPARK));
+            new CANSparkMax(PortMap.RIGHT_FRONT_SPARK, MotorType.kBrushless),
+            new CANSparkMax(PortMap.RIGHT_MIDDLE_SPARK, MotorType.kBrushless),
+            new CANSparkMax(PortMap.RIGHT_BACK_SPARK, MotorType.kBrushless));
         this.drive = new DifferentialDrive(lDrive, rDrive);
     }
 
