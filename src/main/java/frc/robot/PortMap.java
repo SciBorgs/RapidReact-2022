7ae3150b2ee72117673ab3b95c14465156c1582d
warren yun <wyun2006@gmail.com>
2022-01-14 15:46:56 -0500

    Init motors

--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -1,5 +1,20 @@
 package frc.robot;
 
 public class PortMap {
-    
+    //*****************JOYSTICKS*****************//
+
+  public static final int JOYSTICK_LEFT = 1;
+  public static final int JOYSTICK_RIGHT = 0;
+
+  //*******************SPARKS******************//
+
+  public static final int LEFT_FRONT_SPARK = 6; 
+  public static final int LEFT_MIDDLE_SPARK = 5; 
+  public static final int LEFT_BACK_SPARK = 4;
+
+  public static final int RIGHT_FRONT_SPARK = 3;
+  public static final int RIGHT_MIDDLE_SPARK = 2;
+  public static final int RIGHT_BACK_SPARK = 1;
+
+  //*******************SWITCHES******************//
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,13 +1,24 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.PortMap;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class DriveSubsystem extends Subsystem {
-    
-}
+public class DriveSubsystem extends SubsystemBase {
+    public CANSparkMax lFront, lMiddle, lBack, rFront, rMiddle, rBack;
 
+    public DriveSubsystem() {
+        this.lFront = new CANSparkMax(PortMap.LEFT_FRONT_SPARK, MotorType.kBrushless);
+        this.lMiddle = new CANSparkMax(PortMap.LEFT_MIDDLE_SPARK, MotorType.kBrushless);
+        this.lBack = new CANSparkMax(PortMap.LEFT_BACK_SPARK, MotorType.kBrushless);
+
+        this.rFront = new CANSparkMax(PortMap.RIGHT_FRONT_SPARK, MotorType.kBrushless);
+        this.rMiddle = new CANSparkMax(PortMap.RIGHT_MIDDLE_SPARK, MotorType.kBrushless);
+        this.rBack = new CANSparkMax(PortMap.RIGHT_BACK_SPARK, MotorType.kBrushless);
+
+    }
+}
