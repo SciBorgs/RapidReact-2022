7916c4abb2b48b0640b9590b3c226ac64406a34a
warren yun <wyun2006@gmail.com>
2022-01-14 16:19:18 -0500

    Fix dumb errors

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -4,6 +4,6 @@ import frc.robot.PortMap;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class OI{
-    public Joystick JoystickLeft = new Joystick(PortMap.JOYSTICK_LEFT);
-    public Joystick JoysticRight = new Joystick(PortMap.JOYSTICK_RIGHT);
+    public Joystick joystickLeft = new Joystick(PortMap.JOYSTICK_LEFT);
+    public Joystick joysticRight = new Joystick(PortMap.JOYSTICK_RIGHT);
 }
