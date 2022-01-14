ca4177e7e552502e8d35b42711397bea0e00b8c8
Asa Paparo <asapaparo@gmail.com>
2022-01-14 16:42:35 -0500

    first shooter subsystem commit

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+    public CANSparkMax hood, rmotor, lmotor;
+
+    public ShooterSubsystem() {
+        
+    }
+
+    public void shoot() {
+
+    }
+
+    
+}
---
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TurretSubsystem extends SubsystemBase {
+    public CANSparkMax horizontal, vertical;
+
+    public TurretSubsystem() {
+
+    }
+
+    public void aim(int x, int y) {
+
+    }
+
+    private Object find() {
+        
+    }
+}
