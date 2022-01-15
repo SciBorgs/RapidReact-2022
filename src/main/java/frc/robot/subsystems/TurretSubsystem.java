fa0a2cd237e734de25ee66837626515330357134
lee-H1208 <95148272+lee-H1208@users.noreply.github.com>
2022-01-15 13:37:16 -0500

    turret speed (subject to change)

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -5,14 +5,19 @@ import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TurretSubsystem extends SubsystemBase {
-    public CANSparkMax horizontal, vertical;
+    public CANSparkMax turretMotor;
+    //Depends on how many motors there are on turret, but Im thinking there is only one for now.
 
     public TurretSubsystem() {
 
     }
-    
+
     public void aim(int x, int y) {
 
+    }
+
+    public void setTurretSpeed(double speed) {
+        this.turretMotor.set(speed);
     }
 
     public Object find() {
