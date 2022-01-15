510bdcf23d9c93e7cf93b9c04b03590ab0b5f21d
Asa Paparo <asapaparo@gmail.com>
2022-01-15 16:46:11 -0500

    turret subsystem pid

--- src/main/java/frc/robot/commands/turret/AimTurretCommand.java
+++ src/main/java/frc/robot/commands/turret/AimTurretCommand.java
@@ -7,6 +7,6 @@ public class AimTurretCommand extends CommandBase {
     
     @Override
     public void execute() {
-        Robot.limeLightSubsystem.getTableData("tx");
+        Robot.turretSubsystem.turn();
     }
 }
--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,26 +1,24 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import frc.robot.util.PID;
 
 public class TurretSubsystem extends SubsystemBase {
-    public CANSparkMax turretMotor;
+    public CANSparkMax turretMotor = new CANSparkMax(3, MotorType.kBrushless);
     //Depends on how many motors there are on turret, but Im thinking there is only one for now.
+    private PID pid = new PID(1, 1, 1);
 
-    public TurretSubsystem() {
-
-    }
-
-    public void aim(int x, int y) {
-
+    public void turn() {
+        double tx = Robot.limeLightSubsystem.getTableData("tx");
+        double speed = pid.getOutput(0, tx);
+        setTurretSpeed(speed);
     }
 
     public void setTurretSpeed(double speed) {
         this.turretMotor.set(speed);
-    }
-
-    public Object find() {
-        return new Object();
     }
 }
