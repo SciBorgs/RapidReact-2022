ef138fee241358a32e6cb027ba09a1e08b542c3b
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-01-15 15:56:06 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.ClimberSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,6 +20,7 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  public static ClimberSubsystem climberArm = new ClimberSubsystem();
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
