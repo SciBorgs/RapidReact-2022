42058f4cb7ee88bb9309de94e0159ef03fc8d9ab
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-01-19 17:52:57 -0500

    Update ClimberSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -7,10 +7,14 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class ClimberSubsystem implements Subsystem {
     private CANSparkMax climberArm;
+    private CANSparkMax hookMotor;
+
     private final double CLIMBER_ARM_SPEED = 0.5;
+    private final double HOOK_MOTOR_SPEED = 0.2;
 
     public ClimberSubsystem() {
         this.climberArm = new CANSparkMax(PortMap.CLIMBER_ARM, MotorType.kBrushless);
+        this.hookMotor = new CANSparkMax(PortMap.HOOK_MOTOR, MotorType.kBrushless);
     }
 
     public void setClimberArmSpeed() {
@@ -19,6 +23,14 @@ public class ClimberSubsystem implements Subsystem {
 
     public void stopClimberArm() {
         this.climberArm.set(0);
+    }
+
+    public void setHookMotorSpeed() {
+        this.hookMotor.set(this.HOOK_MOTOR_SPEED);
+    }
+
+    public void stopHookMotor() {
+        this.hookMotor.set(0);
     }
 
     public void initDefaultCommand(){
