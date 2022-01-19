742e1d5d7d0bebcbefed6209179f8482d8b54adb
Asa Paparo <asapaparo@gmail.com>
2022-01-19 16:37:50 -0500

    added portmap

---
+++ src/main/PortMap.java
@@ -0,0 +1,9 @@
+public class PortMap {
+    // Shooter
+    public static final int HOOD_SPARK = 0;
+    public static final int SHOOTER_LEFT_SPARK = 1;
+    public static final int SHOOTER_RIGHT_SPARK = 2;
+
+    // Turret
+    public static final int TURRET_SPARK = 4;
+}
