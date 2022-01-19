bc5bae40fa17addd74b356cc2667efe7e067a6f0
lee-H1208 <95148272+lee-H1208@users.noreply.github.com>
2022-01-19 16:45:32 -0500

    changes

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,10 +6,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-    public CANSparkMax hood = new CANSparkMax(0, MotorType.kBrushless);
-    public CANSparkMax lmotor = new CANSparkMax(1, MotorType.kBrushless);
-    public CANSparkMax rmotor = new CANSparkMax(2, MotorType.kBrushless);
-    
+    public CANSparkMax hood, lmotor, rmotor;
+
+    public ShooterSubsystem() {
+        this.hood = new CANSparkMax(0, MotorType.kBrushless);
+        this.lmotor = new CANSparkMax(1, MotorType.kBrushless);
+        this.rmotor = new CANSparkMax(2, MotorType.kBrushless);
+    }
     
     public final double HEIGHTDIFF = 9.8;
     public final double CAM_MOUNT_ANGLE = 34.523;
