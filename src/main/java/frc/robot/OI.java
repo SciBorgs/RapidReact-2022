f07ab09e35c3cb3d99c096b523e00b20ea746d1e
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-01-19 17:53:45 -0500

    Update OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -5,11 +5,14 @@ import frc.robot.commands.*;
 import edu.wpi.first.wpilibj2.command.button.*;
 
 public class OI {
-        public Button climberArm;
+        public Button climberArm, hookMotor;
 
         public OI() {
                 this.climberArm = new Button();
                 this.climberArm.whenHeld(new ClimberExtend());
+
+                this.hookMotor = new Button();
+                this.hookMotor.whenHeld(new HookMotor().withTimeout(0.2));
 
         }
 }
