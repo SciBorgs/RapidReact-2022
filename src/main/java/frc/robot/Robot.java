c15ef76018cf6394563d820727e48bb94e99df1a
Asa Paparo <asapaparo@gmail.com>
2022-01-22 10:58:39 -0500

    limelight stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,6 +36,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    limeLightSubsystem.setCameraParams("Pipeline_Name", 2);
   }
 
   /**
@@ -52,6 +53,8 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    double data = limeLightSubsystem.getTableData("tx");
+    System.out.println(data);
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
