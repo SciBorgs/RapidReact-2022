45e4c4e9c5e0693a950cb5f976513e2eef1e5181
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-01-22 11:03:19 -0500

    Add RETRACT_CLIMBER_ARM

--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -3,5 +3,6 @@ package frc.robot;
 public class PortMap {
     public static final int CLIMBER_ARM = -1;
     public static final int HOOK_MOTOR = -1;
+    public static final int RETRACT_CLIMBER_ARM = -1;
 
 }
