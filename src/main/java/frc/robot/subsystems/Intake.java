e3046d0fca5413c744c7de005944544a3f80d719
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-01-22 13:40:15 -0500

    Updated Intake Arm, new methods
    armSpark -> armSolenoid
    Added two methods to utilize this; extendArm, retractArm
    setSuckSpeed doesn't take any arguments now as the speed now exists in the subsystem as a constant

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,30 +1,37 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import com.revrobotics.CANSparkMax;
 
 import frc.robot.PortMap;
 
 public class Intake implements Subsystem {
 
-    private CANSparkMax armSpark;
+    private DoubleSolenoid armSolenoid;
     private CANSparkMax suckSpark;
 
+    private final double INTAKE_SPEED = 0.5;
+
     public Intake() {
-        this.armSpark = new CANSparkMax(PortMap.INTAKE_ARM_SPARK, CANSparkMax.MotorType.kBrushless);
+        this.armSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, PortMap.INTAKE_ARM_FORWARD_CHANNEL, PortMap.INTAKE_ARM_REVERSE_CHANNEL);
         this.suckSpark = new CANSparkMax(PortMap.INTAKE_SUCK_SPARK, CANSparkMax.MotorType.kBrushless);
     }
 
-    public void setArmSpeed(double speed) {
-        armSpark.set(speed);
+    public void extendArm() {
+        this.armSolenoid.set(DoubleSolenoid.Value.kForward);
     }
 
-    public void setSuckSpeed(double speed) {
-        suckSpark.set(speed);
+    public void setSuckSpeed() {
+        this.suckSpark.set(this.INTAKE_SPEED);
+    }
+
+    public void retractArm() {
+        this.armSolenoid.set(DoubleSolenoid.Value.kReverse);
     }
 
     public void initDefaultCommand(){
 
     }
-
 
 }
