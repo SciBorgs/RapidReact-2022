7b22f8248fef81c4ad615291863443cf2fdc6662
royaboya <roychen1716@gmail.com>
2022-01-22 10:19:46 -0500

    Removed some unnecessary joystick imports

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.PortMap;
