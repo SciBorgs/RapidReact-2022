7f7d48ff5740229f7b82052c6805ef9f0abb17d5
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-01-22 13:41:33 -0500

    Command will **ONLY** extend now
    Probably a one-time use button

--- src/main/java/frc/robot/commands/LowerIntakeArm.java
+++ src/main/java/frc/robot/commands/LowerIntakeArm.java
@@ -5,33 +5,21 @@ import java.util.Set;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Robot;
-import com.revrobotics.CANSparkMax;
-
-//import static frc.robot.Subsystems.intake
 
 public class LowerIntakeArm implements Command {
-
-    private final double INTAKE_SPEED = 0.9;
-
     @Override
     public void initialize() {
-        Robot.intake.setArmSpeed(this.INTAKE_SPEED);
+        Robot.intake.extendArm();
     }
 
     @Override
     public void execute() {
-        
+
     }
 
     @Override
     public boolean isFinished() {
-        return false;
-    }
-    
-    @Override
-    public void end(boolean interrupted) {
-        Robot.intake.setArmSpeed(0);
-        Robot.intake.setIdle(CANSparkMax.IdleMode.kCoast);
+        return true;
     }
 
     @Override
