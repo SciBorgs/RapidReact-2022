eba4bf8c1ac1dcf287d1c9cc623043b59bc68ba7
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-01-22 11:02:02 -0500

    Create RetractClimberArm.java
    Retract Climber Arm

---
+++ src/main/java/frc/robot/commands/RetractClimberArm.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;
+
+import java.util.Set;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Robot;
+
+public class RetractClimberArm implements Command {
+
+    @Override
+    public void initialize() {
+        Robot.climberSubsystem.retractClimberArm();
+    }
+
+    @Override
+    public void execute() {
+        
+    }
+    
+    @Override
+    public void end(boolean interrupted) {
+        Robot.climberSubsystem.stopClimberArm();
+    }
+    
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
