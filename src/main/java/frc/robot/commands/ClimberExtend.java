9eee8bd28c38d21043802b50e515e1e6540f1ca2
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-01-25 16:50:34 -0500

    Update ClimberExtend.java

--- src/main/java/frc/robot/commands/ClimberExtend.java
+++ src/main/java/frc/robot/commands/ClimberExtend.java
@@ -2,14 +2,15 @@ package frc.robot.commands;
 
 import java.util.Set;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Robot;
 
-public class ClimberExtend implements Command {
+public class ClimberExtend extends CommandBase {
 
     @Override
     public void initialize() {
+        this.addRequirements(Robot.climberSubsystem);
         Robot.climberSubsystem.extendClimberArm();
     }
 
