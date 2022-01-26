50c4d4637c51f86a9a17cb2c2493b9b61295829d
SciBorgs Laptop <roboticsbxsci@gmail.com>
2022-01-26 17:48:52 -0500

    properly implemented following (aproved and working) (hopefully)

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.PortMap;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class DriveSubsystem extends SubsystemBase {
@@ -11,18 +12,25 @@ public class DriveSubsystem extends SubsystemBase {
 
     public DriveSubsystem() {
         this.lFront = new CANSparkMax(PortMap.LEFT_FRONT_SPARK, MotorType.kBrushless);
-        this.lMiddle = new CANSparkMax(PortMap.LEFT_MIDDLE_SPARK, MotorType.kBrushless);
+        // this.lMiddle = new CANSparkMax(PortMap.LEFT_MIDDLE_SPARK, MotorType.kBrushless);
         this.lBack = new CANSparkMax(PortMap.LEFT_BACK_SPARK, MotorType.kBrushless);
 
         this.rFront = new CANSparkMax(PortMap.RIGHT_FRONT_SPARK, MotorType.kBrushless);
-        this.rMiddle = new CANSparkMax(PortMap.RIGHT_MIDDLE_SPARK, MotorType.kBrushless);
+        // this.rMiddle = new CANSparkMax(PortMap.RIGHT_MIDDLE_SPARK, MotorType.kBrushless);
         this.rBack = new CANSparkMax(PortMap.RIGHT_BACK_SPARK, MotorType.kBrushless);
 
-        lMiddle.follow(lFront);
+        // lMiddle.follow(lFront);
         lBack.follow(lFront);
         
-        rMiddle.follow(rFront);
+        // rMiddle.follow(rFront);
         rBack.follow(rFront);
+
+        lFront.setIdleMode(IdleMode.kCoast);
+        // lMiddle.setIdleMode(IdleMode.kCoast);
+        lBack.setIdleMode(IdleMode.kCoast);
+        rFront.setIdleMode(IdleMode.kCoast);
+        // rMiddle.setIdleMode(IdleMode.kCoast);
+        rBack.setIdleMode(IdleMode.kCoast);
     }
 
     public void setSpeed(double left, double right) {
@@ -33,7 +41,7 @@ public class DriveSubsystem extends SubsystemBase {
     }
 
     public void setSpeedForwardAngle(double forward, double angle) {
-        System.out.println(forward * (1 + angle));
+        //System.out.println(forward * (1 + angle));
         setSpeed(forward * (1 + angle), forward * (1 - angle)); // thank you zev
     }
 }
