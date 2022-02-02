26a9b1d45258649370721cb1af58819ee32a889e
warren yun <wyun2006@gmail.com>
2022-02-02 17:10:52 -0500

    getYawPitchRoll

--- src/main/java/frc/robot/sciSensorsActuators/SciPigeon.java
+++ src/main/java/frc/robot/sciSensorsActuators/SciPigeon.java
@@ -8,6 +8,16 @@ public class SciPigeon extends PigeonIMU {
         super(id);
     }
 
+    private double[] yawPitchRoll() {
+        double[] yawPitchRollValues = new double[3];
+        super.getYawPitchRoll(yawPitchRollValues);
+        return yawPitchRollValues;
+    }
+
+    public double getAngle() { return Math.toRadians(yawPitchRoll()[0]); }
+    public double getPitch() { return Math.toRadians(yawPitchRoll()[1]); }
+    public double getRoll() { return Math.toRadians(yawPitchRoll()[2]); }
+
     public void setAngle(double angle) {
         super.setYaw(angle);
     }
