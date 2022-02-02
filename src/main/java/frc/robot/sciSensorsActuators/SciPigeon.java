66ccfb54c6e3fdb9c7828e3a7ab0cb0445f9a5ed
warren yun <wyun2006@gmail.com>
2022-02-02 16:38:28 -0500

    Install phoenix + set angle

--- src/main/java/frc/robot/sciSensorsActuators/SciPigeon.java
+++ src/main/java/frc/robot/sciSensorsActuators/SciPigeon.java
@@ -1,3 +1,14 @@
 package frc.robot.sciSensorsActuators;
 
 import com.ctre.phoenix.sensors.PigeonIMU;
+
+public class SciPigeon extends PigeonIMU {
+    
+    public SciPigeon(int id) {
+        super(id);
+    }
+
+    public void setAngle(double angle) {
+        super.setYaw(angle);
+    }
+}
