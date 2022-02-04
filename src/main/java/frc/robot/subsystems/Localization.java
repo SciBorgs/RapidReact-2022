9890dd95ab612fc9d93c473b4ed1aa636fd9964a
warren yun <wyun2006@gmail.com>
2022-02-04 16:27:26 -0500

    Store location

--- src/main/java/frc/robot/subsystems/Localization.java
+++ src/main/java/frc/robot/subsystems/Localization.java
@@ -5,25 +5,29 @@ import edu.wpi.first.wpilibj.Encoder;
 
 import frc.robot.PortMap;
 import frc.robot.sciSensorsActuators.SciPigeon;
+import frc.robot.util.Point;
 
 public class Localization extends SubsystemBase {
-    public SciPigeon pigeon;
+    private Point location;
+    private final int STARTING_X = 0, STARTING_Y = 0;
 
     public Encoder leftEncoder, rightEncoder;
+    public SciPigeon pigeon;
 
     public Localization() {
+        location = new Point(STARTING_X, STARTING_Y);
         pigeon = new SciPigeon(PortMap.PIGEON_ID);
+
         leftEncoder = new Encoder(0, 1);
         rightEncoder = new Encoder(0, 1);
     }
 
-    public int getX() {
-        return -1;
+    public double getX() { return location.getX(); }
+    public double getY() { return location.getY(); }
+
+    public void updateLocation(double x, double y) {
+        location.setX(x);
+        location.setY(y);
     }
 
-    public int getY() {
-        return -1;
-        
-    }
-    
 }
