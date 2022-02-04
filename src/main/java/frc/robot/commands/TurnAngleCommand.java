32822e2252bd78bb0f0fc802d861fe03224b7fc1
miscsb <buenas@bxscience.edu>
2022-02-04 16:12:42 -0500

    finished TurnAngleCommand

--- src/main/java/frc/robot/commands/TurnAngleCommand.java
+++ src/main/java/frc/robot/commands/TurnAngleCommand.java
@@ -1,23 +1,32 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.PortMap;
 import frc.robot.Robot;
 
+import frc.robot.util.PID;
+import frc.robot.util.Point;
+
 public class TurnAngleCommand extends CommandBase {
+    private static final double EPSILON = 1E-6;
+    private PID anglePid;
+    private double targetAngle;
 
-    public void execute() {
-        double dx = 5, dy = 5;
-        double targetAngle = Math.atan2(dy, dx);
-
+    public TurnAngleCommand(Point p) {
+        double dx = p.getX() - Robot.localizationSubsystem.getX();
+        double dy = p.getY() - Robot.localizationSubsystem.getY();
+        this.targetAngle = Math.atan2(dy, dx);
+        this.anglePid = new PID(1, 0, 0);
     }
 
-
+    public void execute() {
+        double currentAngle = Robot.localizationSubsystem.getAngle();
+        double output = this.anglePid.getOutput(targetAngle, currentAngle);
+        Robot.driveSubsystem.setSpeedForwardAngle(0.1, output);
+    }
 
     @Override
     public boolean isFinished() {
-        return false;
+        double currentAngle = Robot.localizationSubsystem.getAngle();
+        return Math.abs(targetAngle - currentAngle) < EPSILON;
     }
 }
--- src/main/java/frc/robot/subsystems/LocalizationSubsystem.java
+++ src/main/java/frc/robot/subsystems/LocalizationSubsystem.java
@@ -6,24 +6,26 @@ import edu.wpi.first.wpilibj.Encoder;
 import frc.robot.PortMap;
 import frc.robot.sciSensorsActuators.SciPigeon;
 
-public class Localization extends SubsystemBase {
+public class LocalizationSubsystem extends SubsystemBase {
     public SciPigeon pigeon;
 
     public Encoder leftEncoder, rightEncoder;
 
-    public Localization() {
+    public LocalizationSubsystem() {
         pigeon = new SciPigeon(PortMap.PIGEON_ID);
         leftEncoder = new Encoder(0, 1);
         rightEncoder = new Encoder(0, 1);
     }
 
-    public int getX() {
+    public double getX() {
         return -1;
     }
 
-    public int getY() {
+    public double getY() {
         return -1;
-        
     }
     
+    public double getAngle() {
+        return pigeon.getAngle();
+    }
 }
