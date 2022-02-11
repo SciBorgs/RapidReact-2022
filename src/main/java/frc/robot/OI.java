e5c84e9e4c1005f855fce337f27773b1e3962cee
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-02-11 17:50:29 -0500

    Update OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -5,12 +5,9 @@ import frc.robot.commands.*;
 import edu.wpi.first.wpilibj2.command.button.*;
 
 public class OI {
-        public Button intakeBalls, lowerIntakeArms, StartHopper, togglePneumatics;
+        public Button lowerIntakeArms, StartHopper, togglePneumatics;
 
         public OI() {
-                this.intakeBalls = new Button();
-                this.intakeBalls.whenHeld(new IntakeBalls());
-
                 this.lowerIntakeArms = new Button();
                 this.lowerIntakeArms.whenPressed(new LowerIntakeArm());
 
