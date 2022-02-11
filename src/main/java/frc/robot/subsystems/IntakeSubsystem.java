30796319243bcbe299bea415e27165ce5f6ae843
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-02-11 17:50:14 -0500

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,42 +1,40 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import com.revrobotics.CANSparkMax;
 
 import frc.robot.PortMap;
 
-public class HopperSubsystem implements Subsystem {
+public class IntakeSubsystem implements Subsystem {
 
-    private CANSparkMax suck;
-    private CANSparkMax elevator;
+    private DoubleSolenoid armSolenoid;
+    private CANSparkMax suckSpark;
+    public DigitalInput limitSwitch;
+    private final double INTAKE_SPEED = 0.5;
 
-    private final double HOOPER_SPEED = 0.5;
+    public IntakeSubsystem() {
+        this.armSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, PortMap.INTAKE_ARM_FORWARD_CHANNEL, PortMap.INTAKE_ARM_REVERSE_CHANNEL);
+        this.suckSpark = new CANSparkMax(PortMap.INTAKE_SUCK_SPARK, CANSparkMax.MotorType.kBrushless);
+        this.limitSwitch = new DigitalInput(PortMap.LIMIT_SWITCH_INTAKE);
+    }
 
-    public HopperSubsystem() {
-        this.suck = new CANSparkMax(PortMap.HOPPER_SUCK_SPARK, CANSparkMax.MotorType.kBrushless);
-        this.elevator = new CANSparkMax(PortMap.HOPPER_ELEVATOR_SPARK, CANSparkMax.MotorType.kBrushless);
+    public void extendArm() {
+        this.armSolenoid.set(DoubleSolenoid.Value.kForward);
     }
 
     public void setSuckSpeed() {
-        this.suck.set(this.HOOPER_SPEED);
+        this.suckSpark.set(this.INTAKE_SPEED);
     }
 
-    public void setElevatorSpeed() {
-        this.elevator.set(this.HOOPER_SPEED);
+    public void retractArm() {
+        this.armSolenoid.set(DoubleSolenoid.Value.kReverse);
     }
 
-    public void stopSuck() {
-        this.suck.set(0);
+    public void initDefaultCommand(){
+
     }
 
-    public void stopElevator() {
-        this.elevator.set(0);
-    }
 
-    public double getSuckSpeed() {
-        return suck.get();
-    }
-
-    public double getElevatorSpeed() {
-        return elevator.get();
-    }
 }
