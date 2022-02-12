3c90063d920b5601be3d17a48eab6c2b17456c37
lee-H1208 <95148272+lee-H1208@users.noreply.github.com>
2022-02-12 15:23:02 -0500

    added absolute encoder -> through bore encoder

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,13 +5,16 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.PortMap;
 import frc.robot.Robot;
-import frc.robot.sciSensorsActuators.SciEncoder;
 import frc.robot.util.PID;
+import edu.wpi.first.wpilibj.Encoder;
+
+import frc.robot.sciSensorsActuators.SciAbsoluteEncoder;
+import frc.robot.sciSensorsActuators.SciEncoder;
 
 public class ShooterSubsystem extends SubsystemBase {
     private static PID pid = new PID(0.04, 0, 0);
     public CANSparkMax hood, lmotor, rmotor;
-    private SciEncoder hoodencoder;
+    private SciAbsoluteEncoder thruBoreEncoder;
     public ShooterSubsystem() {
         /*
         this.hood = new CANSparkMax(PortMap.HOOD_SPARK, MotorType.kBrushless);
@@ -20,11 +23,11 @@ public class ShooterSubsystem extends SubsystemBase {
         */
         
         lmotor.follow(rmotor);
+        //thruBoreEncoder = new SciAbsoluteEncoder(PortMap.THRUBORE_ENCODER, 0.03);
     }
     
     public final double HEIGHTDIFF = 9.8;
     public final double CAM_MOUNT_ANGLE = 34.523;
-    //arbitrary numbers - expect changes after prototype created
 
     public double getDistance(double angle) {
         return HEIGHTDIFF/Math.tan(Math.toRadians(+CAM_MOUNT_ANGLE));
@@ -35,13 +38,14 @@ public class ShooterSubsystem extends SubsystemBase {
     public void moveVert(double speed) {
         hood.set(speed);
     }
+    /*
     public void hoodangle() {
         double ty = Robot.limelightSubsystem.getTableData(Robot.limelightSubsystem.getTable(), "ty");
         double speed = pid.getOutput(ty, 0);
         moveVert(speed);
     }
-    public double getAngle(){
-        return hoodencoder.getDistance();
+    */
+    public void moveHood(double angle) {
+        moveVert(pid.getOutput(angle, thruBoreEncoder.getAngle()));
     }
-    
 }
