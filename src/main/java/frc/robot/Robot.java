0d0351fb88792df6c803f38e553592df749a5319
warren yun <wyun2006@gmail.com>
2022-02-15 08:35:10 -0500

    get table data

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -81,8 +81,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-     //new DriveCommand().execute();
-    System.out.println(PhotonVisionSubsystem.)
+    new DriveCommand().execute();
   }
 
   /** This function is called once when the robot is disabled. */
--- src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
@@ -6,9 +6,16 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// ta: area
+// x: pitch
+
 public class PhotonVisionSubsystem extends SubsystemBase {
     // returns photonvision network table
     public NetworkTable getTable() {
         return NetworkTableInstance.getDefault().getTable("photonvision");
+    }
+
+    public double getTableData(NetworkTable table, String var) {
+        return table.getEntry(var).getDouble(0.0);
     }
 }
