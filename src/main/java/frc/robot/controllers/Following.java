24464664a0f3a05a442529444a44bb31ec317e15
royaboya <roychen1716@gmail.com>
2022-02-16 16:25:13 -0500

    fixing following

--- src/main/java/frc/robot/controllers/Following.java
+++ src/main/java/frc/robot/controllers/Following.java
@@ -1,7 +1,6 @@
 package frc.robot.controllers;
 
 import frc.robot.Robot;
-import frc.robot.util.Averager;
 import frc.robot.util.PID;
 
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -12,12 +11,9 @@ public class Following {
     
     public static final double TX_P = 0.035;
     public static final double TA_P = 0.4;
-    public static final double TA_WEIGHT = 1/10;
-
+    public static final double TA_WEIGHT = 1./10.;
     public static PID txPID, taPID;
     public static double taAvr;
-
-    public static Averager TA_Average = new Averager(50);
 
     public static double tx;
     public static double ta;
@@ -33,14 +29,15 @@ public class Following {
         if (result.hasTargets()) {
             ta = Robot.photonVisionSubsystem.getTarget().getArea();
             tx = Robot.photonVisionSubsystem.getTarget().getYaw();  
-
             taAvr = TA_WEIGHT * ta + (1 - TA_WEIGHT) * taAvr;
             double forward = -taPID.getOutput(Math.exp(-taAvr), 0);
 
-            System.out.println(Math.abs(forward) * 100);
-            Robot.driveSubsystem.setSpeedForwardAngle(forward, txPID.getOutput(tx, 0));
+            System.out.println("Forward: " + Math.abs(forward) * 100);
+            System.out.println("tx: " + tx);
+            System.out.println("taAvg: " + taAvr);
+            //Robot.driveSubsystem.setSpeedForwardAngle(forward, txPID.getOutput(tx, 0));
         } else {
-            Robot.driveSubsystem.setSpeed(0, 0);
+            //Robot.driveSubsystem.setSpeed(0, 0);
         }
 
     }
