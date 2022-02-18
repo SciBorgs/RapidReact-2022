d1c698bde7f3f9bd5360dea1c5131e78bd16d3c8
lee-H1208 <95148272+lee-H1208@users.noreply.github.com>
2022-02-18 13:50:06 -0500

    added offset into bore encoder

--- src/main/java/frc/robot/sciSensorsActuators/SciAbsoluteEncoder.java
+++ src/main/java/frc/robot/sciSensorsActuators/SciAbsoluteEncoder.java
@@ -4,13 +4,17 @@ import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class SciAbsoluteEncoder {
     private DutyCycleEncoder absEncoder;
+    public double offset;
 
     public SciAbsoluteEncoder(int port, double gearRatio) {
         this.absEncoder = new DutyCycleEncoder(port);
         this.absEncoder.setDistancePerRotation(gearRatio * 360);
+        this.offset = 0;
     }
+
+    public void setOffset(double offset) {this.offset = offset;}
+    
     //returns the angle the hood is currently at:
-    public double getAngle() {
-        return absEncoder.getDistance();
-    }
+    public double getAngle() {return absEncoder.getDistance();}
+
 }
