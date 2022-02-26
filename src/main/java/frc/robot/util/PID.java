7200222bff5a1d08a950d69149d2a5b85b9d5086
Samuel Buena <buenas@bxscience.edu>
2022-02-26 18:50:31 -0500

    cleaned up pid

--- src/main/java/frc/robot/util/PID.java
+++ src/main/java/frc/robot/util/PID.java
@@ -3,18 +3,13 @@ package frc.robot.util;
 import edu.wpi.first.wpilibj.Timer;
 
 public class PID {
-  private double p;
-  private double i;
-  private double d;
+  private double p, i, d;
 
-  private double integrator;
-  private double prevErr;
-  private double prevTime;
+  private double integrator, prevErr, prevTime, prevDer;
 
   private Timer timer;
 
-  public PID(double p, double i, double d)
-  {
+  public PID(double p, double i, double d) {
     this.p = p;
     this.i = i;
     this.d = d;
@@ -25,10 +20,19 @@ public class PID {
     timer.start();
   }
 
-  public double getOutput(double setPoint, double processVar)
-  {
-    double err = setPoint - processVar;
+  public double getP() { return this.p; }
+  public double getI() { return this.i; }
+  public double getD() { return this.d; }
 
+  public double getError() { return this.prevErr; }
+  public double getIntegral() { return this.integrator; }
+  public double getDerivative() { return this.prevDer; }
+
+  public void setP(double p) { this.p = p; }
+  public void setI(double i) { this.i = i; }
+  public void setD(double d) { this.d = d; }
+
+  public double getOutput(double err) {
     double der = 0;
     if (prevErr != Double.POSITIVE_INFINITY) {
       der = (err - prevErr) / (timer.get() - prevTime);
@@ -37,7 +41,21 @@ public class PID {
     integrator += err;
     prevErr  = err;
     prevTime = timer.get();
+    prevDer = der;
 
     return p * err + i * integrator + d * der;
+  }
+
+  public double getOutput(double setPoint, double processVar) {
+    return this.getOutput(setPoint - processVar);
+  }
+
+  public double getOutput() {
+    return p * prevErr + i * integrator + d * prevDer;
+  }
+
+  public void reset() {
+    this.integrator = this.prevErr = this.prevTime = 0;
+    this.timer.reset();
   }
 }
