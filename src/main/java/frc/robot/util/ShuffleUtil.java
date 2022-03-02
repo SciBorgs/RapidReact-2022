9836e2bbdaa5352b3aeec15797c1ce1cbb170ee1
mantissav <mantissav@protonmail.com>
2022-03-02 17:52:15 -0500

    Added widgets for tx and ta pid and motors for drive subsystem

---
+++ src/main/java/frc/robot/util/ShuffleUtil.java
@@ -0,0 +1,19 @@
+package frc.robot.util;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+//things that aid the shuffleboard crew (nathanael, dani)
+public class ShuffleUtil {
+    public static void updateEntry(NetworkTableEntry entry, double newValue) {
+        entry.setDouble(newValue);
+    }
+    public static void updateEntries(NetworkTableEntry[] entries, double[] newValues){
+        for(int i = 0; i < entries.length; ++i)
+            entries[i].setDouble(newValues[i]);
+    }
+    public static void updateEntries(NetworkTableEntry[] entries, double[] newValues, int max){
+        for(int i = 0; i < max; ++i)
+            entries[i].setDouble(newValues[i]);
+    }
+}
