34d174aaa921eb44caf7ef08cbb18f7eaee516f2
Asa Paparo <asapaparo@gmail.com>
2022-03-05 11:48:32 -0500

    updated shooter controllers

--- src/main/java/frc/robot/commands/Shooter/AimHoodCommand.java
+++ src/main/java/frc/robot/commands/Shooter/AimHoodCommand.java
@@ -7,11 +7,19 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.PortMap;
 import frc.robot.Robot;
+import frc.robot.controllers.HoodAngleController;
 
 public class AimHoodCommand extends CommandBase {
+    private HoodAngleController hoodAngleController;
+
     @Override
-    public void execute(){
-        Robot.shooterSubsystem.moveHood(HoodAngleController.getDegFromFunction(Robot.shooterSubsystem.getDistance(Robot.limelightSubsystem.getLimelightTableData("ty"))));
+    public void initialize() {
+        hoodAngleController = new HoodAngleController();
+    }
+
+    @Override
+    public void execute() {
+        Robot.shooterSubsystem.moveHood(hoodAngleController.getDegFromFunction(Robot.shooterSubsystem.getDistance(Robot.limelightSubsystem.getLimelightTableData("ty"))));
         double angle = Robot.shooterSubsystem.getHoodAngle();
         System.out.println("Angle: " + angle);
     }
--- src/main/java/frc/robot/controllers/HoodAngleController.java
+++ src/main/java/frc/robot/controllers/HoodAngleController.java
@@ -3,7 +3,15 @@ package frc.robot.controllers;
 import java.util.function.DoubleToLongFunction;
 
 public class HoodAngleController {
-    public double getDegFromFunction(double x){
+    /*
+    Hood angle will be determined from a regression based off test data.
+    This regression will be for the hood angle, using a constant rpm to save time.
+
+    If there is extra time, we should create a regression for the flywheel rpm
+    and then make a new function for hood angle.
+    */
+
+    public double getDegFromFunction(double dist){
         //add later Bozo
         return 1;
     }
