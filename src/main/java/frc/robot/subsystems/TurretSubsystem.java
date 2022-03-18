bd48e4dce046460a600b20a03b240822cfce6e05
Asa Paparo <asapaparo@gmail.com>
2022-03-18 18:20:26 -0400

    removed unused functions of the turret subsystem

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,23 +1,20 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.PortMap;
-import frc.robot.sciSensorsActuators.SciEncoder;
-import frc.robot.sciSensorsActuators.SciPigeon;
-import frc.robot.util.*;
 import frc.robot.Constants;
+import frc.robot.PortMap;
+import frc.robot.sciSensorsActuators.SciAbsoluteEncoder;
+import frc.robot.util.Averager;
+import frc.robot.util.PID;
+import frc.robot.util.ShufflePID;
 
 public class TurretSubsystem extends SubsystemBase {
-    // public CANSparkMax lFront, lMiddle, lBack, rFront, rMiddle, rBack;
-    private SciEncoder encoder;
-    private final int LIMIT = 360;
-    private SciPigeon pigeon;
+    private CANSparkMax motor;
+    private SciAbsoluteEncoder encoder;
 
+    private final int LIMIT = 360;
     private static final double TX_P = 6.0 / 360;
     private ShufflePID pidShuffleboard;
     private PID pid;
@@ -29,49 +26,10 @@ public class TurretSubsystem extends SubsystemBase {
     private static final double DEFAULT_ANGLE = 10;
 
     public TurretSubsystem() {
-       // this.lFront = new CANSparkMax(PortMap.LEFT_FRONT_SPARK, MotorType.kBrushless);
-        // this.lMiddle = new CANSparkMax(PortMap.LEFT_MIDDLE_SPARK, MotorType.kBrushless);
-      //  this.lBack = new CANSparkMax(PortMap.LEFT_BACK_SPARK, MotorType.kBrushless);
-
-       // this.rFront = new CANSparkMax(PortMap.RIGHT_FRONT_SPARK, MotorType.kBrushless);
-        // this.rMiddle = new CANSparkMax(PortMap.RIGHT_MIDDLE_SPARK, MotorType.kBrushless);
-      //  this.rBack = new CANSparkMax(PortMap.RIGHT_BACK_SPARK, MotorType.kBrushless);
-
-        // lMiddle.follow(lFront);
-      //  lBack.follow(lFront);
-        
-        // rMiddle.follow(rFront);
-      //  rBack.follow(rFront);
-
-       // lFront.setIdleMode(IdleMode.kCoast);
-        // lMiddle.setIdleMode(IdleMode.kCoast);
-       // lBack.setIdleMode(IdleMode.kCoast);
-     //   rFront.setIdleMode(IdleMode.kCoast);
-       // // rMiddle.setIdleMode(IdleMode.kCoast);
-      //  rBack.setIdleMode(IdleMode.kCoast);
-
-        
-        this.encoder = new SciEncoder(Constants.SMALL_TURRET_GEAR_RATIO, Constants.WHEEL_CIRCUMFERENCE, lFront.getEncoder());
-        this.pigeon = new SciPigeon(42);
+        this.encoder = new SciAbsoluteEncoder(PortMap.THRUBORE_ENCODER, Constants.SMALL_TURRET_GEAR_RATIO);
         this.pid = new PID(TX_P, 0, 0);
         this.pidShuffleboard = new ShufflePID("Turret", pid, "Main");
         this.txAverager = new Averager(TX_WEIGHT);
-    }
-
-    public void setSpeed(double left, double right) {
-        lFront.set(left * 0.5);
-        rFront.set(-right * 0.5);
-    }
-
-    public void turn(double diff) {
-        setSpeed(diff, -diff);
-    }
-
-    // should returns angle turned from the encoder
-    // using pidgeon for now
-    public double getAngle() {
-        // return encoder.getDistance();
-        return Math.toDegrees(pigeon.getAngle());
     }
 
     // returns direction that the turret is spinning as an int, either 1 or -1
@@ -79,11 +37,6 @@ public class TurretSubsystem extends SubsystemBase {
         if (encoder.getSpeed() > 0)
             return -1;
         return 1;
-    }
-
-    // temporary
-    public void resetPigeon() {
-        pigeon.setAngle(0);
     }
 
     public void pointTowardsTarget(double angle) {
@@ -109,27 +62,3 @@ public class TurretSubsystem extends SubsystemBase {
         pidShuffleboard.update();
     }
 }
-
-
-//for later when we decide to use one motor for the actual turret
-
-/*
-public CANSparkMax susan;
-
-public TurretSubsystem() {
-    this.susan = new CANSparkMax(PortMap.TURRET_SPARK, MotorType.kBrushless);
-    //Depends on how many motors there are on turret, but Im thinking there is only one for now.
-    private PID pid = new PID(1, 1, 1);
-}
-
-public void turn() {
-        double tx = Robot.LimeLightSubsystem.getTableData(Robot.LimeLightSubsystem.getTable(), "tx");
-        double speed = pid.getOutput(0, tx);
-        setTurretSpeed(speed);
-}
-//this might not even be needed, considering we have the followTape controller
-
-public void setTurretSpeed(double speed) {
-        this.susan.set(speed);
-}
-*/
