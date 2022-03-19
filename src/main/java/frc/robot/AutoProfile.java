d4ab4711026592701353868221be6fc6664ecda1
miscsb <buenas@bxscience.edu>
2022-03-18 22:28:29 -0400

    created red paths

--- src/main/java/frc/robot/AutoProfile.java
+++ src/main/java/frc/robot/AutoProfile.java
@@ -3,6 +3,7 @@ package frc.robot;
 import java.util.HashMap;
 import java.util.List;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.util.Path;
 import frc.robot.util.Point;
 import frc.robot.util.Util;
@@ -15,5 +16,15 @@ public class AutoProfile {
     public static final double SHOOTING_RADIUS_FAR  = 3.3;
 
     public static final List<Point> PATH_TEST = Util.generateSinePath(4.0, 0.45, 1.35, 0.05);
-    public static final HashMap<String, Path> paths = new HashMap<>();
+    public static final Point POINT_TEST = new Point(0, 0);
+
+    public static final HashMap<String, Path> AUTON_PATHS = new HashMap<>();
+
+    public boolean initialize() {
+        
+    }
+
+    public Command getCommand() {
+        return null;
+    }
 }
