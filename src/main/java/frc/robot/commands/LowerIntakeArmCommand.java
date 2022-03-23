459444a75938184a8e7a09e35556339bd3290cfc
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-03-23 17:11:52 -0400

    Update LowerIntakeArmCommand.java

--- src/main/java/frc/robot/commands/LowerIntakeArmCommand.java
+++ src/main/java/frc/robot/commands/LowerIntakeArmCommand.java
@@ -7,23 +7,21 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Robot;
 
 public class LowerIntakeArmCommand extends CommandBase {
-    //Extends the intake arm. Runs when pressed button, instant command
     @Override
     public void initialize() {
-        //this.addRequirements(Robot.intake);
-        //Robot.intake.extendArm();
-    }
-
-    @Override
-    public void execute() {
-
+        this.addRequirements(Robot.intakeSubsystem);
+        Robot.intake.startSuck();
     }
 
     @Override
     public boolean isFinished() {
         return true;
     }
+    // useless functions
+    @Override
+    public void execute() {
 
+    }
     @Override
     public Set<Subsystem> getRequirements() {
         // TODO Auto-generated method stub
