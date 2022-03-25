19e14cb7ef8b890285419f39976ca3e83e601580
Asa Paparo <asapaparo@gmail.com>
2022-03-25 19:37:07 -0400

    updated SciAbsolteEncoder

--- src/main/java/frc/robot/sciSensorsActuators/SciAbsoluteEncoder.java
+++ src/main/java/frc/robot/sciSensorsActuators/SciAbsoluteEncoder.java
@@ -4,13 +4,19 @@ import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class SciAbsoluteEncoder {
     private DutyCycleEncoder absEncoder;
+    public double offset;
 
     public SciAbsoluteEncoder(int port, double gearRatio) {
         this.absEncoder = new DutyCycleEncoder(port);
         this.absEncoder.setDistancePerRotation(gearRatio * 360);
     }
+
+    public void setOffset(double offset) {this.offset = offset;}
+    public double getOffset(double angle) {return this.offset = angle - absEncoder.getDistance();}
+    
+
     //returns the angle the hood is currently at:
-    public double getAngle() {
-        return absEncoder.getDistance();
-    }
+    public double getAngle() { return absEncoder.getDistance(); }
+    public void reset() { absEncoder.reset();}
+
 }
