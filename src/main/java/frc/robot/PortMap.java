dde56b9bf8d69e411b606c9af994666a8b3be11b
warren yun <wyun2006@gmail.com>
2022-03-25 19:42:11 -0400

    master portmap

--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -3,32 +3,27 @@ package frc.robot;
 public class PortMap {
   //*****************JOYSTICKS*****************//
 
-  public static final int JOYSTICK_LEFT = 0;
-  public static final int JOYSTICK_RIGHT = 1;
+  public static final int JOYSTICK_LEFT = 1;
+  public static final int JOYSTICK_RIGHT = 0;
 
   //*******************SPARKS******************//
-  // public static final int LEFT_FRONT_SPARK = 26;
-  // public static final int LEFT_MIDDLE_SPARK = 27;
-  // public static final int LEFT_BACK_SPARK = 8;
-
-  // public static final int RIGHT_FRONT_SPARK = 22;
-  // public static final int RIGHT_MIDDLE_SPARK = 15; goobye old bot
-  // public static final int RIGHT_BACK_SPARK = 20;
-
-  public static final int LEFT_FRONT_SPARK = 6;
-  public static final int LEFT_MIDDLE_SPARK = 5;
-  public static final int LEFT_BACK_SPARK = 4;
-
-  public static final int RIGHT_FRONT_SPARK = 3;
-  public static final int RIGHT_MIDDLE_SPARK = 2;
-  public static final int RIGHT_BACK_SPARK = 1;
+  public static final int LEFT_FRONT_SPARK = 0;
+  public static final int LEFT_MIDDLE_SPARK = 0;
+  public static final int LEFT_BACK_SPARK = 0;
+  public static final int RIGHT_FRONT_SPARK = 0;
+  public static final int RIGHT_MIDDLE_SPARK = 0;
+  public static final int RIGHT_BACK_SPARK = 0;
+  
+  public static final int HOOD_SPARK = 28;
+  public static final int FLYWHEEL_LEFT_SPARK = 0;
+  public static final int FLYWHEEL_RIGHT_SPARK = 0;
+  public static final int TURRET_SPARK = 3;
 
   //*******************SWITCHES******************//
   public static final int TOP_LIMIT_SWITCH = 3;
-  public static final int HOOD_SPARK = 28;
   public static final int SHOOTER_LEFT_SPARK = 0;
   public static final int SHOOTER_RIGHT_SPARK = 0;
-  public static final int TURRET_SPARK = 0;
+  public static final int INTAKE_SWITCH = 6;
 
   //*******************MISC******************//
   public static final int PIGEON_ID = 42;
@@ -43,30 +38,17 @@ public class PortMap {
   // *******************HOPPER******************//
   public static final int HOPPER_SUCK_SPARK = -1;
   public static final int HOPPER_ELEVATOR_SPARK = -1;
+
+  // *******************CLIMBER******************//
+
+  public static final int CLIMBER_ARM = -1;
+  public static final int HOOK_MOTOR = -1;
+  public static final int RETRACT_CLIMBER_ARM = -1;
   
   // *******************PNEMUATICS******************//
   public static final int COMPRESSOR = -1;
 
-  //public static final int JOYSTICK_LEFT = 1;
-  //public static final int JOYSTICK_RIGHT = 0;
-
-  //*******************SPARKS******************//
-  //public static final int LEFT_FRONT_SPARK = 26;
-  //public static final int LEFT_MIDDLE_SPARK = 27;
-  //public static final int LEFT_BACK_SPARK = 8;
-
-  //public static final int RIGHT_FRONT_SPARK = 22;
-  //public static final int RIGHT_MIDDLE_SPARK = 15;
-  //public static final int RIGHT_BACK_SPARK = 20;
-
-  // public static final int HOOD_SPARK = 28;
-  // public static final int SHOOTER_LEFT_SPARK = 0;
-  // public static final int SHOOTER_RIGHT_SPARK = 0;
-  // public static final int TURRET_SPARK = 0;
-
-  //*******************SWITCHES******************//
-  //public static final int TOP_LIMIT_SWITCH = 3;
-
   //******************ENCODERS******************//
-   public static final int THRUBORE_ENCODER = 1;
+   public static final int TURRET_ENCODER = 2;
+   public static final int HOOD_ENCODER = 42;
 }
