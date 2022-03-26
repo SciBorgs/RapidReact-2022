41d85acb6ee28f217a4fee8c8ba34803f8d63dd6
Samuel Buena <buenas@bxscience.edu>
2022-03-25 20:47:08 -0400

    incorporated items in util folder from Auto

--- src/main/java/frc/robot/util/ShuffleUtil.java
+++ src/main/java/frc/robot/util/ShuffleUtil.java
@@ -1,19 +1,10 @@
-// package frc.robot.util;
+package frc.robot.util;
 
-// import edu.wpi.first.networktables.NetworkTableEntry;
-// import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.networktables.NetworkTableEntry;
 
-// //things that aid the shuffleboard crew (nathanael, dani)
-// public class ShuffleUtil {
-//     public static void updateEntry(NetworkTableEntry entry, double newValue) {
-//         entry.setDouble(newValue);
-//     }
-//     public static void updateEntries(NetworkTableEntry[] entries, double[] newValues){
-//         for(int i = 0; i < entries.length; ++i)
-//             entries[i].setDouble(newValues[i]);
-//     }
-//     public static void updateEntries(NetworkTableEntry[] entries, double[] newValues, int max){
-//         for(int i = 0; i < max; ++i)
-//             entries[i].setDouble(newValues[i]);
-//     }
-// }
+//things that aid the shuffleboard crew (nathanael, dani)
+public class ShuffleUtil {
+    public static void updateEntry(NetworkTableEntry entry, double newValue) {
+        entry.setDouble(newValue);
+    }
+}
