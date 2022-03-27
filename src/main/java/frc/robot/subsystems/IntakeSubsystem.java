66aa7856740a4583c085d08c10e400827206f190
oliver-ruc <57429193+oliver-ruc@users.noreply.github.com>
2022-03-27 10:36:16 -0400

    Shuffleboard for intake, hopper, pneumatics

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,15 +3,21 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
 import com.revrobotics.CANSparkMax;
 
 import frc.robot.PortMap;
+import frc.robot.Robot;
 
 public class IntakeSubsystem implements Subsystem {
 
     private DoubleSolenoid armSolenoid; // solenoid used for extending and retracting intake arm
     private CANSparkMax suckSpark; // motor used for intaking balls
-    public DigitalInput limitSwitch; // limit switch used for detecting when ball in intake
+    public  DigitalInput limitSwitch; // limit switch used for detecting when ball in intake
+
+    public  ShuffleboardTab intakeTab;
 
     private final double INTAKE_SPEED = 0.5;
 
@@ -20,7 +26,10 @@ public class IntakeSubsystem implements Subsystem {
         this.suckSpark = new CANSparkMax(PortMap.INTAKE_SUCK_SPARK, CANSparkMax.MotorType.kBrushless);
         // this.suckSpark.setInverted(true); // invert the motor
         this.limitSwitch = new DigitalInput(PortMap.LIMIT_SWITCH_INTAKE);
-        
+
+        intakeTab = Shuffleboard.getTab("Intake");
+        intakeTab.addBoolean("Intake Running", Robot.intakeSubsystem::getIntakeRunning);
+        intakeTab.addBoolean("Limit Switch Set", Robot.intakeSubsystem::getSwitchStatus);
     }
 
     public void extendArm() { 
@@ -41,6 +50,10 @@ public class IntakeSubsystem implements Subsystem {
 
     public double getIntakeSpeed() {
         return this.suckSpark.get();
+    }
+
+    public boolean getIntakeRunning() {
+        return (Math.abs(getIntakeSpeed()) < 0.04);
     }
 
     public boolean getSwitchStatus() {
