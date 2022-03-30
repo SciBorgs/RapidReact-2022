cb0121bb6663bf7bd37cf5a08a2c0199eabc3f81
warren yun <wyun2006@gmail.com>
2022-03-29 22:33:59 -0400

    Install shbuffleboard api lib

---
+++ src/main/java/frc/robot/util/widgets/AutoPathWidget.java
@@ -0,0 +1,10 @@
+package frc.robot.util.widgets;
+
+import edu.wpi.first.shuffleboard.api.plugin.Description;
+import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+import edu.wpi.first.shuffleboard.api.widget.ComponentType;
+import edu.wpi.first.shuffleboard.api.widget.WidgetType;
+
+public class AutoPathWidget extends Plugin {
+    
+}
