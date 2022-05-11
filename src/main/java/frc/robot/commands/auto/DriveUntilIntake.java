c2faad0b8dd6cdc396c5853c4863190d73f6ffbd
warren yun <wyun2006@gmail.com>
2022-05-10 22:57:53 -0400

    drive until intake command

--- src/main/java/frc/robot/commands/auto/DriveUntilIntake.java
+++ src/main/java/frc/robot/commands/auto/DriveUntilIntake.java
@@ -1,13 +1,40 @@
 package frc.robot.commands.auto;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.DriveSubsystem.DriveMode;
 
+// Meant to be called at the beginning of Auto in front of ball. Has not been tested yet.
+public class DriveUntilIntake extends CommandBase {
+    private DriveSubsystem drive;
+    private IntakeSubsystem intake;
+    private int currentBallCount;
 
-public class DriveUntilIntake extends ParallelCommandGroup {
-    
     public DriveUntilIntake(DriveSubsystem drive, IntakeSubsystem intake) {
-        
+        this.drive = drive;
+        this.intake = intake;
+        addRequirements(drive, intake);
     }
+
+    @Override
+    public void initialize() {
+        currentBallCount = intake.getBallCount();
+    }
+
+    @Override
+    public void execute() {
+        drive.driveRobot(DriveMode.TANK, -0.2, 0.2); // drive backwards
+    }
+
+    @Override
+    public boolean isFinished() {
+        return intake.getBallCount() == currentBallCount+1;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drive.tankDriveVolts(0, 0);
+    }
+
 }
