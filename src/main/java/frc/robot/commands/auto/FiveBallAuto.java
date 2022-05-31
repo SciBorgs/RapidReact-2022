ea9c0612c032e87b030bbf70c9d46a73f3ae02d8
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-05-30 22:06:57 -0400

    fixed imports (web vscode pls add a linter or something)

--- src/main/java/frc/robot/commands/auto/FiveBallAuto.java
+++ src/main/java/frc/robot/commands/auto/FiveBallAuto.java
@@ -1,11 +1,9 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.commands.HighShot;
-import frc.robot.commands.intake.IntakeForever;
-import frc.robot.commands.intake.IntakeStop;
-import frc.robot.commands.intake.ToggleIntakeArm;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
