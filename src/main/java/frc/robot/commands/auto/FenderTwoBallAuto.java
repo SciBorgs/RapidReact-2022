45dc7695ffb6b26f1653254a47f17ff6d8d94e3d
warren yun <wyun2006@gmail.com>
2022-06-02 13:00:40 -0400

    fender shot in auto

--- src/main/java/frc/robot/commands/auto/FenderTwoBallAuto.java
+++ src/main/java/frc/robot/commands/auto/FenderTwoBallAuto.java
@@ -3,11 +3,13 @@ package frc.robot.commands.auto;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.DriveRamsete;
+import frc.robot.commands.Shoot;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.TurretSubsystem;
+import frc.robot.Constants.ShooterConstants;
 
 public class FenderTwoBallAuto extends SequentialCommandGroup {
     public FenderTwoBallAuto(DriveSubsystem drive, IntakeSubsystem intake, HopperSubsystem hopper, ShooterSubsystem shooter, TurretSubsystem turret, String pos) {
@@ -23,7 +25,15 @@ public class FenderTwoBallAuto extends SequentialCommandGroup {
         );
 
         addCommands(
-            new DriveRamsete(drive, "Pos_" + pos + "_2Ball_Fender", true)
+            new DriveRamsete(drive, "Pos_" + pos + "_2Ball_Fender", true),
+            new Shoot(
+                () -> ShooterConstants.FENDER_RPM,
+                () -> ShooterConstants.FENDER_ANGLE,
+                () -> 0,
+                shooter,
+                turret,
+                hopper
+            )
         );
 
         // add fender shot code here 
