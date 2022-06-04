3ad6dbfb6b092a1f309617c1af70229d195aadd1
warren yun <wyun2006@gmail.com>
2022-06-04 09:46:08 -0400

    fender one ball

---
+++ src/main/java/frc/robot/commands/auto/FenderOneBallAuto.java
@@ -0,0 +1,29 @@
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.commands.DriveRamsete;
+import frc.robot.commands.Shoot;
+import frc.robot.commands.Turn180;
+import frc.robot.subsystems.HopperSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.TurretSubsystem;
+import frc.robot.Constants.ShooterConstants;
+
+public class FenderOneBallAuto extends SequentialCommandGroup {
+    public FenderOneBallAuto(DriveSubsystem drive, IntakeSubsystem intake, HopperSubsystem hopper, ShooterSubsystem shooter, TurretSubsystem turret) {
+        addCommands(
+            new Shoot(
+                () -> ShooterConstants.FENDER_RPM,
+                () -> ShooterConstants.FENDER_ANGLE,
+                () -> 0,
+                shooter,
+                turret,
+                hopper
+            ),
+            new Turn180(drive),
+            new DriveRamsete(drive, "DriveOffTarmac", false)
+        );
+    }
+}
