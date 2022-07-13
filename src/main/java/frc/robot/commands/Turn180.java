a8dc8108a61d1cd2a3fd3f9b11824bed6cc7d854
warren yun <wyun2006@gmail.com>
2022-07-13 00:25:50 -0400

    update turn 180

--- src/main/java/frc/robot/commands/Turn180.java
+++ src/main/java/frc/robot/commands/Turn180.java
@@ -1,33 +1,41 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.DriveSubsystem.DriveMode;
+import edu.wpi.first.math.MathUtil;
 import frc.robot.util.Util;
 
-public class Turn180 extends PIDCommand {
+public class Turn180 extends CommandBase {
+    private DriveSubsystem drive;
+    PIDController turnController;
+
     public Turn180(DriveSubsystem drive) {
-        super(
-                new PIDController(DriveConstants.kP, DriveConstants.kI, DriveConstants.kD),
-                drive::getHeading,
-                Util.normalizeAngle180(drive.getHeading()),
-                // If current heading is less than the desired angle, we spin right. Otherwise, we spin left.
-                output -> drive.driveRobot(DriveMode.TANK, -output, output),
-                        // drive.getHeading() < Util.normalizeAngle180(drive.getHeading()) ? output : -output, 
-                        // drive.getHeading() < Util.normalizeAngle180(drive.getHeading()) ? -output : output),
-                drive
-            );
+        this.drive = drive;
+        addRequirements(drive);
+        turnController = new PIDController(DriveConstants.kP, DriveConstants.kI, DriveConstants.kD);
+        turnController.enableContinuousInput(-180, 180);
+        turnController.setTolerance(2.0);
+    }
 
-        System.out.println("NORMALIZED ANGLE: " + Util.normalizeAngle180(drive.getHeading()));
+    @Override
+    public void initialize() {
+        turnController.reset();
+        turnController.setSetpoint(Util.normalizeAngle180(drive.getHeading()));
+    }
 
-        getController().enableContinuousInput(-180, 180);
-        getController().setTolerance(4);
+    @Override
+    public void execute() {
+        drive.tankDriveVolts(-turnController.calculate(drive.getHeading()), turnController.calculate(drive.getHeading()));
     }
 
     @Override
     public boolean isFinished() {
-        return getController().atSetpoint();
+        return turnController.atSetpoint();
     }
+    public void end(boolean interrupted) {
+        drive.tankDriveVolts(0, 0);
+    }
+
 }
