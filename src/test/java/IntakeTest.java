adf02f699688bece68e473b17a307ef436470d12
warren yun <wyun2006@gmail.com>
2022-10-16 18:31:08 -0400

    junit testing works

--- src/test/java/IntakeTest.java
+++ src/test/java/IntakeTest.java
@@ -1,35 +1,30 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.revrobotics.CANSparkMax;
-
-import org.junit.jupiter.api.*;
-
 import edu.wpi.first.hal.HAL;
-import frc.robot.PortMap;
 import frc.robot.Constants;
 import frc.robot.subsystems.IntakeSubsystem;
+import org.junit.jupiter.api.*;
 
 public class IntakeTest {
-    IntakeSubsystem intake;
-    CANSparkMax motor;
+  IntakeSubsystem intake;
+  CANSparkMax motor;
 
-    @BeforeEach
-    public void setupSubsystem() {
-        assert HAL.initialize(500, 0);
-        intake = new IntakeSubsystem();
-        motor = new CANSparkMax(PortMap.Intake.SUCK_SPARK, CANSparkMax.MotorType.kBrushless);
-    }
+  @BeforeEach
+  public void setupSubsystem() {
+    assert HAL.initialize(500, 0);
+    intake = new IntakeSubsystem();
+  }
 
-    @AfterEach
-    public void destroySubsystem() throws Exception {
-        intake.close();
-    }
+  @AfterEach
+  public void destroySubsystem() throws Exception {
+    intake.close();
+  }
 
-    @DisplayName("blegh")
-    @Test
-    public void set_motors() {
-        intake.startSuck();
-        assertEquals(Constants.IntakeConstants.INTAKE_SPEED, intake.getIntakeSpeed(), 1e-2);
-    }
-
+  @DisplayName("blegh")
+  @Test
+  public void set_motors() {
+    intake.startSuck();
+    assertEquals(Constants.IntakeConstants.INTAKE_SPEED, intake.getIntakeSpeed(), 1e-2);
+  }
 }
